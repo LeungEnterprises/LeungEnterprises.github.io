de100d4529f464a138fc5085e68a26d7ee7b8e45
nathanhleung <nathanhleung@users.noreply.github.com>
2015-11-03 17:43:37 -0500

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,64 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+
+import jade from 'gulp-jade';
+import stylus from 'gulp-stylus';
+import babel from 'gulp-babel';
+
+import flatten from 'gulp-flatten';
+
+gulp.task('jade', () => {
+  return gulp.src(['./**/*.jade', '!_partials/*.jade', '!_layout.jade'])
+    .pipe(plumber())
+    .pipe(jade({
+      basedir: './',
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build'));
+})
+
+gulp.task('stylus', () => {
+  return gulp.src('./css/index.styl')
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('babel', () => {
+  return gulp.src('./js/*.js')
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('copy-img', () => {
+  return gulp.src('./img/**/*')
+    .pipe(gulp.dest('./build/img'));
+});
+
+// Copies all fonts
+gulp.task('fonts', () => {
+  return gulp.src(['./vendor/**/*.{eot,svg,ttf,woff,woff2}', '!./vendor/Ionicons/src/*'])
+    .pipe(flatten())
+    .pipe(gulp.dest('fonts'));
+});
+
+gulp.task('copy-fonts', () => {
+  return gulp.src('./fonts/**/*')
+    .pipe(gulp.dest('./build/fonts'));
+});
+
+// Copies the `vendor` directory into the `build` directory
+gulp.task('copy-vendor', () => {
+  return gulp.src('./vendor/**/*')
+    .pipe(gulp.dest('./build/vendor'));
+});
+
+gulp.task('default', ['jade', 'stylus', 'babel', 'copy-img', 'fonts', 'copy-fonts', 'copy-vendor'], () => {
+  gulp.watch('./**/*.jade', ['jade']);
+  gulp.watch('./css/*.styl', ['stylus']);
+  gulp.watch('./js/*.js', ['babel']);
+
+  gulp.watch('./img/**/*', ['copy-img']);
+  gulp.watch('./vendor/**/*', ['fonts', 'copy-fonts', 'copy-vendor']);
+});
---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(() => {
+  // IE detection; their flexbox implementation is a bit wonky
+  let msie = (function msieversion() {
+    let ua = window.navigator.userAgent;
+    let msie = ua.indexOf("MSIE ");
+
+    return (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./));
+  })();
+
+  if (msie) {
+    $('html').addClass('msie');
+    $('#loader').addClass('hidden');
+    $('#loader-overlay').append('<span class="text-5x text-light">&hellip;</span>')
+  }
+
+  // Preloader Animation
+  $(window).load(() => {
+    $('#loader').fadeOut();
+    $('#loader-overlay').delay(100).fadeOut('slow');
+    $('body').delay(100).css({'overflow':'visible'});
+  });
+
+  // Smooth Scroll from CSS-Tricks
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        let target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 48 // 48 is jumbotron padding
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  // change desktop header on scroll
+  if ($(window).innerWidth() >= 768) {
+    $(window).scroll(() => {
+      if ($(window).scrollTop() > 50) {
+        if (!$('header.desktop').hasClass('open')) {
+          $('header.desktop').addClass('open');
+        }
+      } else {
+        if ($('header.desktop').hasClass('open')) {
+          $('header.desktop').removeClass('open');
+        }
+      }
+    });
+  }
+  // change mobile header on scroll
+  if ($(window).innerWidth() < 768) {
+    $(window).scroll(() => {
+      if ($(window).scrollTop() > 5) {
+        if ($('header.mobile').hasClass('unscrolled')) {
+          $('header.mobile').removeClass('unscrolled');
+        }
+      } else {
+        if (!$('header.mobile').hasClass('unscrolled')) {
+          $('header.mobile').addClass('unscrolled');
+        }
+      }
+    });
+  }
+  // initialize WOW.js, but not on iOS
+  let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if (!iOS) {
+    new WOW().init();
+  }
+});
